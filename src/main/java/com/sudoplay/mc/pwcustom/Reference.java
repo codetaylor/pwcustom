334fba9df77052bace0c4ac65281ce86549e8250
Jason Taylor <jason@codetaylor.com>
2017-12-31 18:04:29 -0800

    modified dependency string

--- src/main/java/com/sudoplay/mc/pwcustom/Reference.java
+++ src/main/java/com/sudoplay/mc/pwcustom/Reference.java
@@ -7,7 +7,7 @@ public class Reference {
   public static final String MOD_ID = "pwcustom";
   public static final String VERSION = "@@VERSION@@";
   public static final String NAME = "PlanesWalker Modpack Custom Content";
-  public static final String DEPENDENCIES = "before:jei;after:athenaeum";
+  public static final String DEPENDENCIES = "after:athenaeum";
 
   public static final boolean IS_DEV = VERSION.equals("@@" + "VERSION" + "@@");
 
