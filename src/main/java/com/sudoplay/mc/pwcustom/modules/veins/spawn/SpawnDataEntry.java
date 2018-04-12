f28dd5ca2121775a06b1e821d448c189c2341c4d
Jason Taylor <jason@codetaylor.com>
2018-04-12 11:03:54 -0700

    Add spawn data entry

--- src/main/java/com/sudoplay/mc/pwcustom/modules/veins/spawn/SpawnDataEntry.java
+++ src/main/java/com/sudoplay/mc/pwcustom/modules/veins/spawn/SpawnDataEntry.java
@@ -1,5 +1,39 @@
 package com.sudoplay.mc.pwcustom.modules.veins.spawn;
 
-public class SpawnDataEntry {
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTUtil;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.common.util.INBTSerializable;
 
+public class SpawnDataEntry
+    implements INBTSerializable<NBTTagCompound> {
+
+  private BlockPos pos;
+  private NBTTagCompound data;
+
+  public SpawnDataEntry() {
+    // serialization
+  }
+
+  public SpawnDataEntry(BlockPos pos, NBTTagCompound data) {
+
+    this.pos = pos;
+    this.data = data;
+  }
+
+  @Override
+  public NBTTagCompound serializeNBT() {
+
+    NBTTagCompound tag = new NBTTagCompound();
+    tag.setTag("pos", NBTUtil.createPosTag(this.pos));
+    tag.setTag("data", this.data);
+    return tag;
+  }
+
+  @Override
+  public void deserializeNBT(NBTTagCompound tag) {
+
+    this.pos = NBTUtil.getPosFromTag(tag.getCompoundTag("pos"));
+    this.data = tag.getCompoundTag("data");
+  }
 }
