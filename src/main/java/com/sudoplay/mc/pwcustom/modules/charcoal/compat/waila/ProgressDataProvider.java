9c49abfad0e3a4ad6ced48030dc9aa6f1a48b490
Jason Taylor <jason@codetaylor.com>
2018-04-22 07:54:55 -0700

    Improve kiln tooltips

--- src/main/java/com/sudoplay/mc/pwcustom/modules/charcoal/compat/waila/ProgressDataProvider.java
+++ src/main/java/com/sudoplay/mc/pwcustom/modules/charcoal/compat/waila/ProgressDataProvider.java
@@ -1,17 +1,21 @@
 package com.sudoplay.mc.pwcustom.modules.charcoal.compat.waila;
 
 import com.sudoplay.mc.pwcustom.modules.charcoal.ModuleCharcoal;
-import com.sudoplay.mc.pwcustom.modules.charcoal.tile.IProgressProvider;
+import com.sudoplay.mc.pwcustom.modules.charcoal.block.BlockKiln;
+import com.sudoplay.mc.pwcustom.modules.charcoal.recipe.KilnRecipe;
+import com.sudoplay.mc.pwcustom.modules.charcoal.tile.TileKiln;
 import com.sudoplay.mc.pwcustom.util.Util;
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -48,18 +52,69 @@ public class ProgressDataProvider
   ) {
 
     if (config.getConfig(WailaRegistrar.CONFIG_PROGRESS)
-        && accessor.getTileEntity() instanceof IProgressProvider) {
+        && accessor.getTileEntity() instanceof TileKiln) {
 
-      IProgressProvider progressProvider = (IProgressProvider) accessor.getTileEntity();
-      float progress = progressProvider.getProgress();
+      IBlockState blockState = accessor.getBlockState();
+      TileKiln tileKiln = (TileKiln) accessor.getTileEntity();
+      float progress = tileKiln.getProgress();
+      ItemStackHandler stackHandler = tileKiln.getStackHandler();
+      ItemStack input = stackHandler.getStackInSlot(0);
+      KilnRecipe recipe = KilnRecipe.getRecipe(input);
+      ItemStack output = ItemStack.EMPTY;
 
-      if (progress > 0f) {
-        tooltip.add(Util.translateFormatted(
-            "gui." + ModuleCharcoal.MOD_ID + ".waila.casting.progress",
-            Util.DF_PERCENT.format(progress)
-        ));
+      StringBuilder renderString = new StringBuilder();
+
+      if (recipe != null) {
+        output = recipe.getOutput();
       }
+
+      if (blockState.getValue(BlockKiln.VARIANT) == BlockKiln.EnumType.COMPLETE) {
+
+        tooltip.add(Util.translate("gui." + ModuleCharcoal.MOD_ID + ".waila.kiln.pit.finished"));
+
+        ItemStackHandler outputStackHandler = tileKiln.getOutputStackHandler();
+        StringBuilder outputString = new StringBuilder();
+        boolean hasOutput = false;
+
+        for (int i = 0; i < outputStackHandler.getSlots(); i++) {
+          ItemStack stackInSlot = outputStackHandler.getStackInSlot(i);
+
+          if (!stackInSlot.isEmpty()) {
+            outputString.append(WailaUtil.getStackRenderString(stackInSlot));
+            hasOutput = true;
+          }
+        }
+
+        tooltip.add("");
+        tooltip.add(Util.translate("gui." + ModuleCharcoal.MOD_ID + ".waila.result"));
+
+        if (hasOutput) {
+          renderString.append(outputString.toString());
+
+        } else {
+          renderString.append(Util.translate("gui." + ModuleCharcoal.MOD_ID + ".waila.empty"));
+        }
+
+      } else {
+
+        if (input.isEmpty()) {
+          renderString.append(Util.translate("gui." + ModuleCharcoal.MOD_ID + ".waila.empty"));
+
+        } else {
+
+          if (blockState.getValue(BlockKiln.VARIANT) == BlockKiln.EnumType.WOOD) {
+            tooltip.add(Util.translate("gui." + ModuleCharcoal.MOD_ID + ".waila.kiln.pit.ready"));
+          }
+
+          renderString.append(WailaUtil.getStackRenderString(input));
+          renderString.append(WailaUtil.getProgressRenderString((int) (100 * progress), 100));
+          renderString.append(WailaUtil.getStackRenderString(output));
+        }
+      }
+
+      tooltip.add(renderString.toString());
     }
+
     return tooltip;
   }
 
