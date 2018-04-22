1d7380ff95686955d7f019b7975dbef4a7c18366
Jason Taylor <jason@codetaylor.com>
2018-04-22 07:02:23 -0700

    Change mod display name

--- src/main/java/com/sudoplay/mc/pwcustom/Reference.java
+++ src/main/java/com/sudoplay/mc/pwcustom/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
 
   public static final String MOD_ID = "pwcustom";
   public static final String VERSION = "@@VERSION@@";
-  public static final String NAME = "PlanesWalker Modpack Custom Content";
+  public static final String NAME = "PWCustom";
   public static final String DEPENDENCIES = "after:athenaeum";
 
   public static final boolean IS_DEV = VERSION.equals("@@" + "VERSION" + "@@");
